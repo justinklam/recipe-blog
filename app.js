5a782232555dbd341e177277536f670d9d00cfea
Justin Lam <justin.kh.lam@gmail.com>
2022-05-03 15:47:36 -0400

    create server, add in port. add in middleware

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+
+const app = express();
+const port = normalizePort(process.env.PORT || "3000");
+console.log(process.env.PORT);
+
+require("dotenv").config();
+
+// middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public")); // for static files
+app.use(expressLayouts);
+
+app.set("layout", "./layouts/main");
+
+// routes
+const routes = require("./server/routes/recipeRoutes.js");
+app.use("/", routes);
+
+app.listen(port, () =>
+  console.log(`Recipe-blog is listening on Port:${port} 🐯`)
+);
+
+function normalizePort(val) {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
