95587319eb27d42e1f2ad3e5191fc57c755e61ea
Justin Lam <justin.kh.lam@gmail.com>
2022-05-07 17:30:13 -0400

    change typography to topology in database.js and .env file to resolve mongodb connection error

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -1,8 +1,52 @@
+require ('../models/database');
+const Category = require('../models/Category')
+
 /**
  * GET /
  * Homepage
  */
 
+
+
 exports.homepage = async (req, res) => {
   res.render("index", {title: 'Recipe Repo - Home'});
 };
+
+async function insertDummyCategoryData() {
+  try {
+    await Category.insertMany([
+  {
+    "name": "Chinese",
+    "image": "food-chinese.jpg"
+  },
+  {
+    "name": "French",
+    "image": "food-french.jpg"
+  },
+  {
+    "name": "Italian",
+    "image": "food-italian.jpg"
+  },
+  {
+    "name": "Japanese",
+    "image": "food-japanese.jpg"
+  },
+  {
+    "name": "Mexican",
+    "image": "food-mexican.jpg"
+  },
+  {
+    "name": "Spanish",
+    "image": "food-spanish.jpg"
+  },
+  {
+    "name": "Vietnamnese",
+    "image": "food-vietnamnese.jpg"
+  },
+]);
+  } catch(error) {
+    console.log('Error', + error);
+  }
+};
+
+insertDummyCategoryData();
