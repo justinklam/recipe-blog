9fe96c8ad41500d958177f3dc4e05faf72df9605
Justin Lam <justin.kh.lam@gmail.com>
2022-05-08 12:22:05 -0400

    add in Recipe schema

--- server/models/Recipe.js
+++ server/models/Recipe.js
@@ -5,7 +5,25 @@ const recipeSchema = new mongoose.Schema({
     type: String,
     required: [true, 'This field is required']
   },
+  description: {
+    type: String,
+    required: [true, 'This field is required']
+  },
+  ingredients: {
+    type: Array,
+    required: [true, 'This field is required']
+  },
+  category: {
+    // Array causes error
+    type: String,
+    enum: ['Chinese', 'French', 'Italian', 'Japanese', 'Mexican', 'Spanish', 'Vietnamese'],
+    required: [true, 'This field is required']
+  },
   image: {
+    type: String,
+    required: [true, 'This field is required']
+  },
+  email: {
     type: String,
     required: [true, 'This field is required']
   },
