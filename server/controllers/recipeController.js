e17068df38ff583f231fddeaa171d19a101efe6e
Justin Lam <justin.kh.lam@gmail.com>
2022-05-07 21:04:09 -0400

    comment out dummy data insertion

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -9,44 +9,48 @@ const Category = require('../models/Category')
 
 
 exports.homepage = async (req, res) => {
-  res.render("index", {title: 'Recipe Repo - Home'});
-};
-
-async function insertDummyCategoryData() {
   try {
-    await Category.insertMany([
-  {
-    "name": "Chinese",
-    "image": "food-chinese.jpg"
-  },
-  {
-    "name": "French",
-    "image": "food-french.jpg"
-  },
-  {
-    "name": "Italian",
-    "image": "food-italian.jpg"
-  },
-  {
-    "name": "Japanese",
-    "image": "food-japanese.jpg"
-  },
-  {
-    "name": "Mexican",
-    "image": "food-mexican.jpg"
-  },
-  {
-    "name": "Spanish",
-    "image": "food-spanish.jpg"
-  },
-  {
-    "name": "Vietnamnese",
-    "image": "food-vietnamnese.jpg"
-  },
-]);
+    res.render("index", {title: 'Recipe Repo - Home'});
   } catch(error) {
-    console.log('Error', + error);
+
   }
 };
 
-insertDummyCategoryData();
+// async function insertDummyCategoryData() {
+//   try {
+//     await Category.insertMany([
+//   {
+//     "name": "Chinese",
+//     "image": "food-chinese.jpg"
+//   },
+//   {
+//     "name": "French",
+//     "image": "food-french.jpg"
+//   },
+//   {
+//     "name": "Italian",
+//     "image": "food-italian.jpg"
+//   },
+//   {
+//     "name": "Japanese",
+//     "image": "food-japanese.jpg"
+//   },
+//   {
+//     "name": "Mexican",
+//     "image": "food-mexican.jpg"
+//   },
+//   {
+//     "name": "Spanish",
+//     "image": "food-spanish.jpg"
+//   },
+//   {
+//     "name": "Vietnamnese",
+//     "image": "food-vietnamnese.jpg"
+//   },
+// ]);
+//   } catch(error) {
+//     console.log('Error', + error);
+//   }
+// };
+
+// insertDummyCategoryData();
