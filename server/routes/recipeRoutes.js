30c234d36555856ae44813fc8a5b3d15e4919670
Justin Lam <justin.kh.lam@gmail.com>
2022-05-10 16:32:22 -0400

    add in explore-random route. change href in main page to explore-random

--- server/routes/recipeRoutes.js
+++ server/routes/recipeRoutes.js
@@ -10,6 +10,7 @@ router.get("/", recipeController.homepage);
 router.get("/categories", recipeController.exploreCategories);
 router.get("/categories/:id", recipeController.exploreCategoriesById);
 router.get("/explore-latest", recipeController.exploreLatest);
+router.get('/explore-random', recipeController.exploreRandom);
 router.get('/recipe/:id', recipeController.exploreRecipe);
 
 router.post('/search', recipeController.searchRecipe);
