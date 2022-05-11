adc2959fc2a4fc2e1e71f72bbd166c566d7c4c5b
Justin Lam <justin.kh.lam@gmail.com>
2022-05-11 14:14:25 -0400

    add in event listeners to add in directions

--- public/js/script.js
+++ public/js/script.js
@@ -2,6 +2,10 @@ let addIngredientsBtn = document.getElementById('addIngredientsBtn');
 let ingredientList = document.querySelector('.ingredientList');
 let ingredientDiv = document.querySelectorAll('.ingredientDiv')[0];
 
+let addDirectionsBtn = document.getElementById('addDirectionsBtn');
+let directionList = document.querySelector('.directionList');
+let directionDiv = document.querySelectorAll('.directionDiv')[0];
+
 // on click event listener
 addIngredientsBtn.addEventListener('click', function(){
   // clones the node
@@ -10,4 +14,14 @@ addIngredientsBtn.addEventListener('click', function(){
   let input = newIngredients.getElementsByTagName('input')[0];
   input.value = '';
   ingredientList.appendChild(newIngredients);
+});
+
+// on click event listener
+addDirectionsBtn.addEventListener('click', function(){
+  // clones the node
+  let newDirections = directionDiv.cloneNode(true);
+  // gets the first input
+  let input = newDirections.getElementsByTagName('textarea')[0];
+  input.value = '';
+  directionList.appendChild(newDirections);
 });
