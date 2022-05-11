40d6b221b91353989bac87546ce45ec954352219
Justin Lam <justin.kh.lam@gmail.com>
2022-05-11 14:20:39 -0400

    install cookieParser. add in fileUpload, session and flash into app.js

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
+const fileUpload = require('express-fileupload');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const flash = require('connect-flash');
 
 const app = express();
 const port = normalizePort(process.env.PORT || "3000");
