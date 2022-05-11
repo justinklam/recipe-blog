95fa1eba723d31d4f9ee8b2c99316ae3aad7450f
Justin Lam <justin.kh.lam@gmail.com>
2022-05-11 14:08:39 -0400

    add in event listener and handlers for adding ingredients

--- public/js/script.js
+++ public/js/script.js
@@ -1,2 +1,13 @@
 let addIngredientsBtn = document.getElementById('addIngredientsBtn');
 let ingredientList = document.querySelector('.ingredientList');
+let ingredientDiv = document.querySelectorAll('.ingredientDiv')[0];
+
+// on click event listener
+addIngredientsBtn.addEventListener('click', function(){
+  // clones the node
+  let newIngredients = ingredientDiv.cloneNode(true);
+  // gets the first input
+  let input = newIngredients.getElementsByTagName('input')[0];
+  input.value = '';
+  ingredientList.appendChild(newIngredients);
+});
