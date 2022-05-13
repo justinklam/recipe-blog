b89cfbac02c9beda869f90702fff4e45e0cd158e
Justin Lam <justin.kh.lam@gmail.com>
2022-05-13 15:09:12 -0400

    add require dotenv into database.js

--- server/models/database.js
+++ server/models/database.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+require("dotenv").config();
+
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
