b19b8c04a4494a6745725ceb61238f4013912c63
Justin Lam <justin.kh.lam@gmail.com>
2022-05-13 15:39:03 -0400

    update dotenv.config path

--- server/models/database.js
+++ server/models/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-require("dotenv").config();
+require("dotenv").config({path: '.env'});
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
